1e73afca02fb11e29b053caca82d233c8ee1e665
Reuben Ogbonna <reubenogbonna@gmail.com>
2019-11-14 00:07:40 -0500

    added project files

---
+++ exercises.js
@@ -0,0 +1,15 @@
+
+
+// The below code connects this JS fil to our test file
+module.exports = {
+  isRealPalindrome,
+  runningTotal,
+  swap,
+  wordSizes,
+  union,
+  firstRecurring,
+  showMultiplicativeAverage,
+  multiplyList,
+  sequence,
+  wordCap,
+}
