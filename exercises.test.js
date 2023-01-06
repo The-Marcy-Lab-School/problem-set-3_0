07d430503dbf5a033bab6b20a79eb8ce1ca1b47d
Ann Duong <30392423+thuyanduong@users.noreply.github.com>
2023-01-06 05:08:21 -0500

    Update exercises.test.js

--- exercises.test.js
+++ exercises.test.js
@@ -6,6 +6,7 @@ test('Question 1: isRealPalindrome', () => {
   expect(exercises.isRealPalindrome('Madam')).toBe(true);
   expect(exercises.isRealPalindrome("Madam, I'm Adam")).toBe(true);
   expect(exercises.isRealPalindrome('356653')).toBe(true);
+  expect(exercises.isRealPalindrome('356a653')).toBe(true);
   expect(exercises.isRealPalindrome('123ab321')).toBe(false);
 });
 
